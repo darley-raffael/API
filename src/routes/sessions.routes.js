93cec71d3518833ffae32d1850cb59954fab5ab0
Dárley Raffael <darleyraffael@gmail.com>
2023-01-25 21:47:27 -0300

    adicionado rota de autenticação

---
+++ src/routes/sessions.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const SessionsController = require("../controllers/SessionsController");
+const sessionsController = new SessionsController(); // Instanciando a função
+
+const sessionsRouter = Router();
+
+sessionsRouter.post("/", sessionsController.create );
+
+
+module.exports = sessionsRouter;
