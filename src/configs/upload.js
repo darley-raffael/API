c413998a532cfd0ca8e4758f8ea11f2c992e740e
Dárley Raffael <99445232+darley-raffael@users.noreply.github.com>
2023-02-08 19:48:47 +0000

    alterações no eslint

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -7,19 +7,19 @@ const UPLOAD_FOLDER = path.resolve(__dirname, "uploads");
 
 
 const MULTER = {
-    storage: multer.diskStorage({
-        destination: TMP_FOLDER,
-        filename(request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString("hex");
-            const fileName = `${fileHash}-${file.originalname}`;
+	storage: multer.diskStorage({
+		destination: TMP_FOLDER,
+		filename(request, file, callback) {
+			const fileHash = crypto.randomBytes(10).toString("hex");
+			const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
-        },
-    }),
+			return callback(null, fileName);
+		},
+	}),
 };
 
 module.exports = {
-    TMP_FOLDER,
-    UPLOAD_FOLDER,
-    MULTER
+	TMP_FOLDER,
+	UPLOAD_FOLDER,
+	MULTER
 };
--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 const { hash, compare } = require("bcryptjs");
 
 const AppError = require("../utils/AppError");
@@ -6,18 +7,18 @@ const sqliteConnection = require("../database/sqlite");
 // const { use } = require("express/lib/router");
 
 class UsersController {
-	async create (request, response){
+	async create(request, response) {
 		const { name, email, password } = request.body;
 
 		const database = await sqliteConnections();
 		const checkUsersExists = await database
 			.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-		if(checkUsersExists){
-			throw new AppError ("Este email já está em uso.");
+		if (checkUsersExists) {
+			throw new AppError("Este email já está em uso.");
 		}
 
-		const hashPassword = await hash(password,8);
+		const hashPassword = await hash(password, 8);
 
 		await database.run("INSERT INTO users (name, email, password) VALUES(?, ?, ?)",
 			[name, email, hashPassword]
@@ -26,32 +27,32 @@ class UsersController {
 		return response.status(201).json();
 	}
 
-	async update (request, response){
+	async update(request, response) {
 		const { name, email, password, old_password } = request.body;
 		const user_id = request.user.id;
 
 		const database = await sqliteConnection();
 		const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
 
-		if(!user){
+		if (!user) {
 			throw new AppError("Usuário não encontrado");
 		}
 
 		const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-		if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+		if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
 			throw new AppError("Este email já está em uso.");
 		}
 		user.name = name ?? user.name; // Nullish coalescing operator (??)
 		user.email = email ?? user.email;
 
-		if(password && !old_password){
+		if (password && !old_password) {
 			throw new AppError("Informe a senha antiga para atualizar");
 		}
-		if(password && old_password){
+		if (password && old_password) {
 			const checkOldPassword = await compare(old_password, user.password);
 
-			if(!checkOldPassword){
+			if (!checkOldPassword) {
 				throw new AppError("A senha antiga não confere.");
 			}
 
@@ -65,7 +66,7 @@ class UsersController {
             password = ?,
             update_at = DATETIME('now')
             WHERE id = ?`,
-		[user.name, user.email, user.password, user_id]
+			[user.name, user.email, user.password, user_id]
 		);
 
 		return response.status(200).json();
