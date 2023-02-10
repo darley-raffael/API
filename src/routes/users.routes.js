7c1d5dd6d2ef0a80caefffcca14e3e81327ad713
Dárley Raffael <99445232+darley-raffael@users.noreply.github.com>
2023-02-10 21:43:24 +0000

    finalizando as rotas  de update avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,20 +1,21 @@
 const {Router} = require("express");
-const UsersController = require("../controllers/UsersController");
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const multer = require("multer");
 const uploadConfig = require("../configs/upload");
+
+
+const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig.MULTER);
 
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/" , ensureAuthenticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
-	console.log(request.file.filename);
-	response.json();
-});
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
